b3dfe7db901c13b7e774a0e39601d45828e7225c
Julien <julienbroens@gmail.com>
2020-06-03 13:55:02 +0200

    récupération de l'ensemble des arbres via mongoose

--- src/server/index.js
+++ src/server/index.js
@@ -7,50 +7,60 @@
  */
 
 // UTILE :
-// // "mongodb://dev:dev@mongo:27017/"
+// // "mongodb://dev:dev@mongo:27017/crack-hall-three"
 
 import express from "express";
 import path from "path";
-import {ServerResponse} from "http";
-import bodyParser from "body-parser";
 
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
 const {APP_PORT} = process.env;
+const mongoose = require("mongoose");
 
 const app = express();
+const uri = "mongodb://mongo:27017/";
+const options = {
+    useNewUrlParser: true,
+    dbName: "crack-hall-three",
+    user: "dev",
+    pass: "dev",
+};
 
-// Connection URL :
-const url =
-    "mongodb://dev:dev@mongo:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
-// const url = 'mongodb://$[username]:$[password]@$[hostlist]/$[database]?authSource=$[authSource]';
-// mongodb://dev:dev@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false
+mongoose.connect(uri, options, function (error) {
+    // Check error in initial connection. There is no 2nd param to the callback.
+    console.log(" !!!! OK  !!!");
+});
+
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+    // we're connected!
+    console.log("==> CONNECTE <==");
+    let threeSchema = new mongoose.Schema({
+        nom_complet: String,
+        arbotag: Number,
+        geoloc: {
+            lat: Number,
+            lon: Number,
+        },
+        hauteur_totale: Number,
+        diametre_cime: Number,
+        circonf: Number,
+    });
+
+    let Three = mongoose.model("threes", threeSchema);
+    Three.find(function (err, threes) {
+        if (err) return console.error(err);
+        console.log(threes);
+    });
+});
+
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
 app.get("/hello", (req, res) => {
-    console.log("tu es dans Hello");
+    console.log("==> TU ES DANS HELLO");
 });
 
-app.get("/test", (req, res) => {
-    MongoClient.connect(url, function (err, client) {
-        assert.equal(null, err);
-        const db = client.db("crack-hall-three");
-        let cursor = db.collection("three").find({});
-        cursor.forEach(iterateFunc, errorFunc);
-        client.close();
-    });
-    function iterateFunc(doc) {
-        //console.log(doc);
-
-        console.log(JSON.stringify(doc, null, 4));
-
-        res.json(doc);
-    }
-
-    function errorFunc(error) {
-        console.log("==> ERROR");
-        console.log(error);
-    }
+app.get("/allthrees", (req, res) => {
+    console.log("==> TU ES DANS ALL-THREES");
 });
 
 app.listen(APP_PORT, () =>
